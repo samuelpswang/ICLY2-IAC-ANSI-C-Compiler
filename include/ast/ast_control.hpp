8c289d3bd9e428fc94b82e8d77c9a4515e05a21c
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-03-02 17:48:34 +0000

    feat: add while ast

--- include/ast/ast_control.hpp
+++ include/ast/ast_control.hpp
@@ -8,27 +8,6 @@
 
 #include "ast_node.hpp"
 
-class IfElse: public Node {
-public:
-    IfElse(const Node* expression, const Node* truebranch, const Node* falsebranch) {
-        this->type = "ifelse";
-        this->name = "";
-        this->val = "";
-        this->exprs = { expression };
-        this->stats = { truebranch, falsebranch };
-    }
-    void print(std::ostream& os, const std::string& indent) const {
-        os << indent << "if (" << this->exprs[0] << ") {" << std::endl;
-        this->stats[0]->print(os, indent+"\t");
-        os << indent << "} else {" << std::endl;
-        this->stats[1]->print(os, indent+"\t");
-        os << indent << "}" << std::endl;
-    }
-    void compile(std::ostream& os, const std::string& dest) const {
-        os << "ifelse: not implemented" << std::endl;
-    }
-};
-
 class If: public Node {
 public:
     If(const Node* expression, const Node* truebranch) {
@@ -39,12 +18,58 @@ public:
         this->stats = { truebranch};
     }
     void print(std::ostream& os, const std::string& indent) const {
-        os << indent << "if (" << this->exprs[0] << ") {" << std::endl;
+        os << indent << "if (";
+        this->exprs[0]->print(os, "");
+        os << ") {" << std::endl;
         this->stats[0]->print(os, indent+"\t");
         os << indent << "}" << std::endl;
     }
     void compile(std::ostream& os, const std::string& dest) const {
         os << "if: not implemented" << std::endl;
+    }
+};
+
+class IfElse: public Node {
+public:
+    IfElse(const Node* expression, const Node* truebranch, const Node* falsebranch) {
+        this->type = "ifelse";
+        this->name = "";
+        this->val = "";
+        this->exprs = { expression };
+        this->stats = { truebranch, falsebranch };
+    }
+    void print(std::ostream& os, const std::string& indent) const {
+        os << indent << "if (";
+        this->exprs[0]->print(os, "");
+        os << ") {" << std::endl;
+        this->stats[0]->print(os, indent+"\t");
+        os << indent << "} else {" << std::endl;
+        this->stats[1]->print(os, indent+"\t");
+        os << indent << "}" << std::endl;
+    }
+    void compile(std::ostream& os, const std::string& dest) const {
+        os << "ifelse: not implemented" << std::endl;
+    }
+};
+
+class While: public Node {
+public:
+    While(const Node* expression, const Node* branch) {
+        this->type = "while";
+        this->name = "";
+        this->val = "";
+        this->exprs = { expression };
+        this->stats = { branch };
+    }
+    void print(std::ostream& os, const std::string& indent) const {
+        os << indent << "while (";
+        this->exprs[0]->print(os, "");
+        os << ") {" << std::endl;
+        this->stats[0]->print(os, indent+"\t");
+        os << "}" << std::endl;
+    }
+    void compile(std::ostream& os, const std::string& dest) const {
+        os << "while: not implemented" << std::endl;
     }
 };
 
