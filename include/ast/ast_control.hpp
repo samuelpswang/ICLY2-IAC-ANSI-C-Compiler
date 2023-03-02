d8a50f3c0ac36d258507bf5f720937d728c5e959
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2023-03-02 17:43:40 +0000

    feat: added If statement

--- include/ast/ast_control.hpp
+++ include/ast/ast_control.hpp
@@ -29,4 +29,23 @@ public:
     }
 };
 
+class If: public Node {
+public:
+    If(const Node* expression, const Node* truebranch) {
+        this->type = "if";
+        this->name = "";
+        this->val = "";
+        this->exprs = { expression };
+        this->stats = { truebranch};
+    }
+    void print(std::ostream& os, const std::string& indent) const {
+        os << indent << "if (" << this->exprs[0] << ") {" << std::endl;
+        this->stats[0]->print(os, indent+"\t");
+        os << indent << "}" << std::endl;
+    }
+    void compile(std::ostream& os, const std::string& dest) const {
+        os << "ifelse: not implemented" << std::endl;
+    }
+};
+
 #endif
