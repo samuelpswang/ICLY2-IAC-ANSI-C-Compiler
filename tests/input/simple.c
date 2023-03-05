2e0dfac03bc0d3b2f3df9242fe4c6e2f8de3ade8
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2023-03-05 20:04:40 +0000

    feat: added test example to test new features

--- tests/input/simple.c
+++ tests/input/simple.c
@@ -1,2 +1,33 @@
+int ref(){
+    int a = 5;
+}
+
+
 int main(){
-int a = 5;int b = 6;}
+    int a = 5;int b = 6;
+    int c = ((a+b)/(a-b)+b);  
+    int d = (a >> 5)*(6);
+    int e = a < b;
+    int e = a>b;
+    int e = (a!=b);
+    int f = (a == b);
+    int g = a|b;
+    int h = -(a^b);
+    int i = a&&b;
+    int j = a||b;
+    a = b;
+
+    if(a <b ){
+        a = 5;
+    }
+
+    else{
+        a = 6;
+    }
+
+    while(a<6){
+        int a = 5;
+        a = 5;
+    }
+}
+
