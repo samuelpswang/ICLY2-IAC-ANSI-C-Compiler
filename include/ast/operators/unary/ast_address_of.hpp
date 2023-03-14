5470efc81ad278687f33d90e8974d31fec435a7c
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-03-14 10:10:31 +0000

    feat: add pointer operations

---
+++ include/ast/operators/unary/ast_address_of.hpp
@@ -0,0 +1,25 @@
+#ifndef compiler_ast_operators_unary_address_of
+#define compiler_ast_operators_unary_address_of
+
+#include "ast.hpp"
+using namespace std;
+
+class AddressOfOperator: public Node {
+public:
+    // Constructor
+    AddressOfOperator(Node* expression): \
+        Node{"addressof", "", "", expression, nullptr} {}
+
+    // Members
+    void print(std::ostream& os, const std::string& indent) const {
+        os << indent << "&";
+        this->exprs[0]->print(os, "");
+    };
+    void compile(std::ostream& os, const std::string& dest, MemoryContext& m) const {
+        string symbol_name = this->exprs[0]->get_name();
+        int stack_offset = m.get_symbol(symbol_name);
+        os << "\taddi " << dest << ", sp, " << to_string(stack_offset) << "\n";
+    };
+};
+
+#endif
